9322c3dcb6f5ed6a89b88fba13580fa43fe6a512
nafarmer <nafarmer@cisco.com>
2020-03-10 21:30:36 -0400

    Trivial changes

--- index_documents.py
+++ index_documents.py
@@ -2,7 +2,13 @@
 #
 # Step 2: Run this file to create the document index on your local machine.
 
+import os
+
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
-ps = PorterStemmer()
+
+if __name__ == '__main__':
+    # Crawl the documents and add each term to the inverted index
+
+    ps = PorterStemmer()
 

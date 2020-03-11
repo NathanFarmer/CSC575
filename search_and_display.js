0bef20649b1202149573c35097d7a105f23ad2f5
nafarmer <nafarmer@cisco.com>
2020-03-11 09:28:50 -0400

    Documentation

---
+++ search_and_display.js
@@ -0,0 +1,5 @@
+// J. Nathan Farmer, Rohit Kothari, Sachinder Katoch
+//
+// Takes search query as input and feeds it to retrieive_documents.py. Retrieived documents are then
+// returned to this file for display in the browser.
+

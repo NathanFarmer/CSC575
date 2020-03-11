053c691720b9a064bc790debcf22a1f699835141
nafarmer <nafarmer@cisco.com>
2020-03-10 23:34:53 -0400

    Spacing

--- index_documents.py
+++ index_documents.py
@@ -13,7 +13,7 @@ class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
         self.strict = False
-        self.convert_charrefs= True
+        self.convert_charrefs = True
         self.fed = []
     def handle_data(self, d):
         self.fed.append(d)

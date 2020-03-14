6dbb0fcbd8dfbffed2fdbd6a5a391e4beecc4f66
Nathan Farmer <jnathanfarmer@gmail.com>
2020-03-14 15:09:23 -0400

    load_relevance function

--- retrieval.py
+++ retrieval.py
@@ -5,6 +5,10 @@ def retrieve_documents(q):
     if q:
         return {10901322:'data/ajepidem/10901322.html', 10901323:'data/ajepidem/10901323.html'}
 
+def load_relevance():
+    # Loads the predefined relevance information
+    return None
+
 def load_index():
     # This function loads the index from index.json
     with open('data/index.json', 'r') as read_file:
@@ -15,6 +19,7 @@ if __name__ == 'retrieval' or __name__ == '__main__':
     # Weather we are running this file as a standalone or importing it
     # from somewhere else we still want to go ahead and load the index
     index = load_index()
+    relevance = load_relevance()
 
 if __name__ == '__main__':
     # If we are running this file as a standalone we can use this block for
